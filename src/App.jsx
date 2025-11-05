334275cbd8082890a9073fd4b15396a8c70f2bc3
Stenly Adam <stenly.adam@unklab.ac.id>
2025-11-05 09:25:16 +0800

    click event & state

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,35 @@
 //Fragment (<> </>) adalah container seperti <div> yang tidak bisa distyling
 //dimana fungsi utamanya adalah sebagai container
 //Javascript expression inside JSX menggunakan ({ })
+import { useState } from "react";
 import HelloWorld from "./HelloWorld";
 
 const App = () => {
-  let hello = "Hello World";
+  // let hello = "Hello World"; //ini adalah variabel biasa
+  let [hello, setState] = useState("Hello World"); //ini adalah variabel state
+
+  const handleClick = () => {
+    console.log("Button di klik");
+    // hello = "Selamat Datang"; //ini cara merubah variabel biasa
+    setState("Selamat Datang"); //ini cara merubah variabel state
+    // console.log(hello);
+  };
+
   return (
     <>
       <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
         {hello}
       </div>
-      <HelloWorld text="Ini adalah komponen Hello World 1" />
-      <HelloWorld text="Ini adalah komponen Hello World 2" />
+      <HelloWorld
+        text="Ini adalah komponen Hello World 1"
+        handleClick={handleClick}
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 py-2 px-5 text-white rounded-xl"
+        onClick={handleClick}
+      >
+        Click Me
+      </button>
     </>
   );
 };
--- src/HelloWorld/index.jsx
+++ src/HelloWorld/index.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import HelloMoon from "../HelloMoon";
 
 //tipe data props adalah Object
 //pros read only
-const HelloWorld = ({ text }) => {
+const HelloWorld = ({ text, handleClick }) => {
+  const [welcome, setWelcome] = useState("Welcome");
+  const handleChildClick = () => {
+    setWelcome("Selamat Datang");
+  };
   return (
     <>
       <div className="text-[25px] bg-red-500 m-10 p-10 font-serif">
         {text}
+        {welcome}
         <HelloMoon />
+        <button
+          className="bg-green-500 hover:bg-green-700 py-2 px-5 text-white rounded-xl"
+          onClick={handleChildClick}
+        >
+          Click Me
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 py-2 px-5 text-white rounded-xl"
+          onClick={handleClick}
+        >
+          Click Me
+        </button>
       </div>
     </>
   );
